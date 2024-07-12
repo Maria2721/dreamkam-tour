05423c8a186cc605743bedf9c0c001188248bfa8
Maria2721 <mashaivvas@gmail.com>
2024-07-12 13:40:56 +0300

    The project structure was created, fonts and general styles were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from 'react'
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import styles from './App.module.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Dreamkam Tour</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <div className={styles.App}>
+            <Header/>
+            <main>
+                <h1>Dreamkam Tour</h1>
+                <div>
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                </div>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
